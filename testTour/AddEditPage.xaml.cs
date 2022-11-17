9dc19fb70f7a2822444ef44085e0b6b5245df0a5
user02 <user01@user.02>
2022-11-17 09:48:24 +0300

    Добавьте файлы проекта.

---
+++ testTour/AddEditPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testTour
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditPage.xaml
+    /// </summary>
+    public partial class AddEditPage : Page
+    {
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage(Hotel selectedHotel)
+        {
+            InitializeComponent();
+            if (selectedHotel != null)
+                _currentHotel = selectedHotel;
+            DataContext = _currentHotel;
+            ComboCountries.ItemsSource = ToursEntities.GetContext().Country.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentHotel.Name))
+                errors.AppendLine("Укажите название отеля");
+            if(_currentHotel.CountOfStars<1||_currentHotel.CountOfStars>5)
+                errors.AppendLine("Количество звезд - число от 1 до 5");
+            if (_currentHotel.Country == null)
+                errors.AppendLine("Выберите страну");
+            if(errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentHotel.Id == 0)
+                ToursEntities.GetContext().Hotel.Add(_currentHotel);
+            try
+            {
+                ToursEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+    }
+}
---
+++ testTour/HotelsPage.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testTour
+{
+    /// <summary>
+    /// Логика взаимодействия для HotelsPage.xaml
+    /// </summary>
+    public partial class HotelsPage : Page
+    {
+        public HotelsPage()
+        {
+            InitializeComponent();
+            DGridHotel.ItemsSource = ToursEntities.GetContext().Hotel.ToList();
+        }
+
+
+
+        private void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var hotelsForRemoving = DGridHotel.SelectedItems.Cast<Hotel>().ToList();
+            if(MessageBox.Show($"Вы точно хотите удалить следующие {hotelsForRemoving.Count()}элементов?", "Внимание",
+                    MessageBoxButton.YesNo, MessageBoxImage.Question)==MessageBoxResult.Yes)
+            {
+                try
+                {
+                    ToursEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    ToursEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                    DGridHotel.ItemsSource= ToursEntities.GetContext().Hotel.ToList();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+                
+            
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                ToursEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotel.ItemsSource = ToursEntities.GetContext().Hotel.ToList();
+
+            }
+        }
+    }
+}
